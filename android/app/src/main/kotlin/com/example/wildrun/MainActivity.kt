b662a0fb682d1cb727e3e7090d6f02129ae49cc6
ranto <jranto003@gmail.com>
2024-02-23 19:50:29 +0300

    setup project

---
+++ android/app/src/main/kotlin/com/example/wildrun/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wildrun
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/game/wildrun.dart
@@ -0,0 +1,12 @@
+import 'package:flame/events.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:hive/hive.dart';
+import 'package:flame/parallax.dart';
+import 'package:flutter/material.dart';
+import 'package:flame/components.dart';
+
+class DinoRun extends FlameGame with TapDetector, HasCollisionDetection {
+  DinoRun({super.camera});
+}
